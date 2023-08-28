5aa7e87b87c0f1f7af5a25a3943474fba68e955d
Margarita Makarova <ya.margarita.karimova@gmail.com>
2023-08-28 17:29:06 +0200

    Changes

--- final_work/pageobjects/personalPage.js
+++ final_work/pageobjects/personalPage.js
@@ -6,7 +6,6 @@ class PersonalPage extends BasePage {
     this.purchases = '.uc-purchase';
     this.personalAccount = '.uc-nav.uc-nav-last [href="/personal/account.phtml"]+sup';
     this.cashback = '.uc-allprops';
-    this.cart = '.uc-nav [href="/checkout/"]';
     this.favoritesTab = '#user-tab-wishlist';
     this.favoritesWrapper = '.search-info-results__content'
   }
