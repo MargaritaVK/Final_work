71fb9b60ebd4e48217de79c2ea9b452e82c5ca38
Margarita Makarova <ya.margarita.karimova@gmail.com>
2023-08-28 17:44:20 +0200

    Changes

--- final_work/pageobjects/productPage.js
+++ final_work/pageobjects/productPage.js
@@ -4,12 +4,10 @@ const baseElement = require('../helpers/baseelement');
 class ProductPage extends BasePage {
   constructor() {
     super();
-    this.productTitle = '.b-product-title';
     this.productImage = '//*[contains(@class,"picture")]//*[@data-size="380x575"]//img';
     this.productName = '.b-product-title h1';
     this.addToCartButton = '.addtocart-btn';
     this.afterAddButton = '.second-button';
-    this.comments = '.rm-review';
     this.commentsDate = '.b-comment__date';
     this.addCommentButton = '.b-comment-new__state-btn';
     this.commentContainer = '.b-comment-new__tab.b-comment-new__tab_review';
